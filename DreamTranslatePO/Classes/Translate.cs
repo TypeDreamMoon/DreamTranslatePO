1165bb14a9760f42f5a63531fcae8ffa84d702c8
TypeDreamMoon <1067823908@qq.com>
2025-05-03 17:27:41 +0800

    添加翻译引擎类

---
+++ DreamTranslatePO/Classes/Translate.cs
@@ -0,0 +1,62 @@
+﻿namespace DreamTranslatePO.Classes
+{
+    namespace Translate
+    {
+        public class TranslateEngineSetting
+        {
+            public string? Icon;
+            public string TranslateEngineName;
+            public string ApiUrl;
+            public string ApiKey;
+            public string ServerUrl;
+            public bool RequiresAnApi;
+            public bool ServerMode;
+
+            public TranslateEngineSetting()
+            {
+                ApiUrl = "null";
+                ApiKey = "null";
+                ServerUrl = "null";
+                RequiresAnApi = false;
+                ServerMode = false;
+                TranslateEngineName = "null";
+            }
+        }
+
+        /// <summary>
+        /// Translate Engine Impl
+        /// </summary>
+        public class TranslateEngine
+        {
+        }
+
+        public class TranslateEngineAi : TranslateEngine
+        {
+        }
+
+        public class TranslateEngineMicrosoft : TranslateEngine
+        {
+        }
+        
+        public class TranslateEngineGoogle : TranslateEngine
+        {
+        }
+        
+        public class TranslateEngineBaidu : TranslateEngine
+        {
+        }
+        
+        
+        public class TranslateEngineDeepL : TranslateEngine
+        {
+        }
+        
+        public class TranslateEngineYouDao : TranslateEngine
+        {
+        }
+        
+        public class TranslateEngineTencent : TranslateEngine
+        {
+        }
+    }
+}
